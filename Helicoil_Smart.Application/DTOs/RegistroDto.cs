7e89a9e01ea16ce0e2ed3eaebeef116705cef2c9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-07-30 19:17:07 -0500

    Add RegistroDto class

---
+++ Helicoil_Smart.Application/DTOs/RegistroDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Helicoil_Smart.Application.DTOs
+{
+    public class RegistroDto
+    {
+        public string Nombre { get; set; }
+        public string Cedula { get; set; }
+        public DateTime FechaHora { get; set; }
+        public double Latitud { get; set; }
+        public double Longitud { get; set; }
+    }
+}
