4a73b8ac035cf6877ab300ea6f1e5c81a7748886
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-07-30 19:31:02 -0500

    Add AppDbContext

---
+++ Helicoil_Smart.Infrastructure/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace Helicoil_Smart.Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options) { }
+
+        public DbSet<Registro> Registros { get; set; }
+    }
+}
