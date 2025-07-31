e7431f1db16eafb621957f21d0b8110fa86fa2a9
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-07-30 19:51:20 -0500

    Fix program

--- Helicoil_Smart.API/Program.cs
+++ Helicoil_Smart.API/Program.cs
@@ -1,3 +1,8 @@
+using Helicoil_Smart.Application.Interfaces;
+using Helicoil_Smart.Application.Services;
+using Helicoil_Smart.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
