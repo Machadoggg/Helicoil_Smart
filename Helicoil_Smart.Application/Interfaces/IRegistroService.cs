8c93e07382b2c91645aaef25eabfd2f7afee5d3d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2025-07-30 19:19:15 -0500

    Add IRegistroService interface

---
+++ Helicoil_Smart.Application/Interfaces/IRegistroService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helicoil_Smart.Application.Interfaces
+{
+    using Application.DTOs;
+    using Domain.Entities;
+
+    public interface IRegistroService
+    {
+        Task GuardarRegistroAsync(RegistroDto dto);
+        Task<byte[]> ExportarExcelAsync(string? nombre, string? cedula, DateTime? desde, DateTime? hasta);
+    }
+
+}
